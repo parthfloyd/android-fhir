aba29b2c9b402f281053bb7289fc356e4e6bc5db
Parth panchal <40067366+parthfloyd@users.noreply.github.com>
2022-11-07 09:44:43 +0530

    reverted hapiFhir Version change

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -17,7 +17,7 @@ dependencies {
 
   implementation("com.spotify.ruler:ruler-gradle-plugin:1.2.1")
 
-  implementation("ca.uhn.hapi.fhir:hapi-fhir-structures-r4:6.1.3")
+  implementation("ca.uhn.hapi.fhir:hapi-fhir-structures-r4:6.0.1")
   implementation("com.squareup:kotlinpoet:1.9.0")
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.6.21")
   implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.6.10")
