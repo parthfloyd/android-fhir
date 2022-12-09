c9dbe5b7eeca8db5073c146f130e73315f6044cc
Parth Panchal <ppanchal@argusoft.in>
2022-12-09 15:38:52 +0530

    - hidden validation highlight bug

--- demo/src/main/java/com/google/android/fhir/demo/AddPatientFragment.kt
+++ demo/src/main/java/com/google/android/fhir/demo/AddPatientFragment.kt
@@ -82,8 +82,7 @@ class AddPatientFragment : Fragment(R.layout.add_patient_fragment) {
   private fun addQuestionnaireFragment() {
     val fragment = QuestionnaireFragment()
     fragment.arguments =
-      bundleOf(QuestionnaireFragment.EXTRA_QUESTIONNAIRE_JSON_STRING to viewModel.questionnaire
-      , QuestionnaireFragment.EXTRA_ENABLE_REVIEW_PAGE to true)
+      bundleOf(QuestionnaireFragment.EXTRA_QUESTIONNAIRE_JSON_STRING to viewModel.questionnaire)
     childFragmentManager.commit {
       add(R.id.add_patient_container, fragment, QUESTIONNAIRE_FRAGMENT_TAG)
     }
