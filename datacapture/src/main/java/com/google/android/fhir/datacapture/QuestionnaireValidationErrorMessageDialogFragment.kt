66e4bdc04a74087ee3d93982d6ca6cb6e324baa5
Parth Panchal <ppanchal@argusoft.in>
2022-12-19 13:29:08 +0530

    -removed unused import

--- datacapture/src/main/java/com/google/android/fhir/datacapture/QuestionnaireValidationErrorMessageDialogFragment.kt
+++ datacapture/src/main/java/com/google/android/fhir/datacapture/QuestionnaireValidationErrorMessageDialogFragment.kt
@@ -27,7 +27,6 @@ import androidx.core.content.res.use
 import androidx.core.text.toSpanned
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.setFragmentResult
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.fhir.datacapture.validation.Invalid
